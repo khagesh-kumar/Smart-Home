efe80d229e6085ce3884b0690abfe40c8dbf4e5e
Bhadrachalam khagesh kumar <159632032+khagesh-kumar@users.noreply.github.com>
2025-01-21 00:03:25 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,14 +4,14 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyDADg_e-wFFtUa8EDl_sL8au10IvNGDPhg",
+  authDomain: "homesurveillancesystem-1f07a.firebaseapp.com",
+  databaseURL: "https://homesurveillancesystem-1f07a-default-rtdb.firebaseio.com",
+  projectId: "homesurveillancesystem-1f07a",
+  storageBucket: "homesurveillancesystem-1f07a.firebasestorage.app",
+  messagingSenderId: "546290862917",
+  appId: "1:546290862917:web:292746635e4e038d8d8c2c",
+  measurementId: "G-9SXMK25PRH"
 };
 
 // Initialize Firebase
